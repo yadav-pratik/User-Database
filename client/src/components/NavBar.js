94637409dfaa53de77682f10bec7bcb778600a48
pratik <yadavpratik8806@gmail.com>
2022-12-28 18:30:59 +0530

    client : added conditional rendering inside Navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const NavBar = (props) => {
+    const isLogged = useSelector((state)=> {
+        return state.isLogged
+    })
+
     return (
         <div>
             <h1>Users Portal</h1>
+            {isLogged ? (
+                    <div>
+                        <span>Welcome user</span>
+                        <span>Logout</span>
+                    </div>
+                ) : (
+                    <div>
+                        Register
+                    </div>
+                )
+            }
         </div>
     )
 }
