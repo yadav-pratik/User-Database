198733f7ad0d8ddd11e9b39ef531a739cd956071
pratik <yadavpratik8806@gmail.com>
2022-12-28 19:09:41 +0530

    client : added basic fields inside Register component

--- client/src/components/authentication/Register.js
+++ client/src/components/authentication/Register.js
@@ -1,9 +1,67 @@
-import React from "react"
+import React, { useState } from "react"
 
 const Register = (props) => {
+    const [name, setName] = useState('')
+    const [mobile, setMobile] = useState('')
+    const [loginId, setLoginId] = useState('')
+    const [password, setPassword] = useState('')
+    const [country, setCountry] = useState('')
+
+    const handleChange = (e) => {
+        const name = e.target.name
+        if(name === 'name'){
+            setName(e.target.value)
+        } else if(name === 'mobile'){
+            setMobile(e.target.value)
+        } else if(name === 'loginId'){
+            setLoginId(e.target.value)
+        } else if(name === 'password'){
+            setPassword(e.target.value)
+        }
+    }
     return (
         <div>
-            Register
+            <form>
+                <label>Enter your Name</label>
+                <br/>
+                <input
+                    type="text"
+                    value = {name}
+                    name = "name"
+                    onChange = {handleChange}
+                />
+                <br/>
+                <label>Enter your Mobile no.</label>
+                <br/>
+                <input
+                    type="text"
+                    value = {mobile}
+                    name = "mobile"
+                    onChange = {handleChange}
+                />
+                <br/>
+                <label>Enter a Login Id</label>
+                <br/>
+                <input
+                    type="text"
+                    value = {loginId}
+                    name = "loginId"
+                    onChange = {handleChange}
+                />
+                <br/>
+                <label>Enter a Password</label>
+                <br/>
+                <input
+                    type="text"
+                    value = {password}
+                    name = "password"
+                    onChange = {handleChange}
+                />
+                <br/>
+                <select value={country} name = "country" onChange={handleChange}>
+                    <option value = "">Select your Country</option>
+                </select>
+            </form>
         </div>
     )
 }
