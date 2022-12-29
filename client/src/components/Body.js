c8fad9f5ef08a2dd5a6abb13083db4a2352a31cd
pratik <yadavpratik8806@gmail.com>
2022-12-29 13:48:17 +0530

    client : added PrivateRouter component for UserPage

--- client/src/components/Body.js
+++ client/src/components/Body.js
@@ -6,6 +6,8 @@ import Login from "./authentication/Login";
 import Register from "./authentication/Register";
 import UserPage from "./user/UserPage";
 
+import PrivateRoute from "./authentication/PrivateRoute";
+
 const Body = (props) => {
     const isLogged = useSelector((state)=>{
         return state.isLogged
@@ -14,7 +16,8 @@ const Body = (props) => {
         <div>
             <Route path="/" component={isLogged ? UserPage : Login} exact/>
             <Route path="/register" component={Register} exact/>
-            <Route path="/user-page" component={UserPage} exact/>
+            {/* <Route path="/user-page" component={UserPage} exact/> */}
+            <PrivateRoute path="/user-page" component={UserPage} exact/>
         </div>
     )
 }
